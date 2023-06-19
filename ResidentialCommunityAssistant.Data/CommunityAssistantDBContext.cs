669f042ea4bd86e3a1dee60de1d2443aea302d35
Todor Todorov <t.todorov135@gmail.com>
2023-06-19 13:35:39 +0300

    Add project files.

---
+++ ResidentialCommunityAssistant.Data/CommunityAssistantDBContext.cs
@@ -0,0 +1,13 @@
+﻿namespace ResidentialCommunityAssistant.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CommunityAssistantDBContext : IdentityDbContext
+    {
+        public CommunityAssistantDBContext(DbContextOptions<CommunityAssistantDBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
